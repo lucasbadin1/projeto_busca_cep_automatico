1620f9f1f398d90cb235aa94752375efa9e01a7d
Lucas <lucasprado.cnpq@natura.net>
2024-12-26 20:39:09 -0300

    Projeto busca automática de CEP

---
+++ script.js
@@ -0,0 +1,100 @@
+
+
+//fetch('https://viacep.com.br/ws/06653020/json/')
+//  .then((respose) => {
+//    //console.log(respose);
+//    respose.json().then((data) => {
+//        console.log(data);
+// });
+//})
+//  .catch ((erro) => {
+//    console.log(erro);
+//});
+
+
+// Com async e await
+
+
+// async function obterCep() {
+//     try {
+//         const resposta = await fetch('https://viacep.com.br/ws/06653020/json/');
+
+//         const data = await resposta.json();
+//         console.log(data);
+//     } catch (erro) {
+//         console.log("Cep não encontrado");
+//     } finally {
+//         console.log("terminou a requisição")
+//     }
+// };
+
+// obterCep();
+
+
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('uf').value = ("");
+    document.getElementById('ibge').value = ("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('rua').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('uf').value = (conteudo.uf);
+        document.getElementById('ibge').value = (conteudo.ibge);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+function pesquisacep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('uf').value = "...";
+            document.getElementById('ibge').value = "...";
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+    }
+};
